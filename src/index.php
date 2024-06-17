f62168fb48ad525166aa0e2d7055f09f6a74c350
l-guerra-rago <lylian.gr@protonmail.com>
2024-06-17 09:54:55 +0200

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+  require_once 'components/head.php';
+  require_once 'components/header.php';
+?>
+
+<!doctype html>
+<html lang="en">
+
+  <?php
+    render_head('test')
+  ?>
+
+  <body>
+
+    <?php
+      render_header()
+    ?>
+
+  </body>
+</html>
