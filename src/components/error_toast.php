f2c18fffadf11d0f88375e6764c32064a9e8c690
l-guerra-rago <lylian.gr@protonmail.com>
2024-06-19 10:28:02 +0200

    change notif style

--- src/components/error_toast.php
+++ src/components/error_toast.php
@@ -13,11 +13,13 @@
         $error = $_GET['error'];
         
         //echo tailwind css toast
-        echo "<div class='absolute right-0 bg-red-500 text-white font-bold rounded-lg border shadow-lg p-4 m-4 flex flex-row gap-16'>
-            <p class='text-center'>$error</p>
+        echo "<div class='absolute right-0 bg-slate-900 text-white font-bold rounded-lg border-4 border-red-500 shadow-lg p-4 m-4 flex flex-row gap-16'>
+            <div class='flex flex-col justify-center'>
+                <p class='text-center'>$error</p>
+            </div>
             <a href='?'>
-                <button class='bg-red-700 hover:bg-red-600 text-white font-bold py-2 rounded focus:outline-none focus:shadow-outline px-16' type='button'>
-                    Ok
+                <button class='bg-slate-900 hover:bg-slate-700 text-red-500 font-bold py-2 rounded focus:outline-none focus:shadow-outline px-6' type='button'>
+                    X
                 </button>
             </a>
         </div>";
