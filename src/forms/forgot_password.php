2b1b6b4025d6bb1891207eb9273629abf6956cc2
l-guerra-rago <lylian.gr@protonmail.com>
2024-06-19 11:52:33 +0200

    add mail send and add phpmailer librairy

--- src/forms/forgot_password.php
+++ src/forms/forgot_password.php
@@ -1,4 +1,14 @@
 <?php
+    
+    use db\models\Users;
+    use lib\orm\Query;
+    use PHPMailer\PHPMailer\PHPMailer;
+    
+    $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+    require_once $root . '/PHPMailer/PHPMailer.php';
+    require_once $root . '/lib/orm/Query.php';
+    require_once $root . '/db/models/Users.php';
+    
     session_start();
     
     require_once "../db/models/RecoveryToken.php";
@@ -28,17 +38,50 @@
     $token_model->save();
     
     $html_msg = '
-        <a href="http://'.$server_url.'/reset_password.php?token='.$token_model->data['token'].'">
-            Reset password
-        </a>
+    <div class="container">
+        <h1>Skystock</h1>
+        <p>Click the link below to reset your password</p>
+        <a href="http://' . $server_url . '/reset_password.php?token=' . $token . '">Reset password</a>
+    </div>
     ';
     
-    mail(
-        $username,
-        'Forgot password',
-        $html_msg
+    $mail = new PHPMailer(true);
+    
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'mailcatcher';
+        $mail->SMTPAuth = false;
+        $mail->Port = 1025;
+    }
+    catch (Exception $e) {
+        echo 'Error setting up mailer';
+        exit();
+    }
+    
+    $mail->setFrom(
+        'support@skystock.com',
+        'Support',
     );
     
-    echo 'Email sent';
-    echo '<a href="/login.php">Back to login</a>';
-    echo $html_msg;
+    $userQuery = new Query(new Users());
+    $user = $userQuery->where('username', $username)->get()->first();
+    
+    $mail->addAddress(
+        $user->data['email'],
+        $user->data['username']
+    );
+    
+    $mail->isHTML(true);
+    
+    $mail->Subject = 'Password recovery';
+    
+    $mail->Body = $html_msg;
+    
+    try {
+        $mail->send();
+    } catch (Exception $e) {
+        echo 'Error sending email';
+        exit();
+    }
+    
+    header('Location: /login.php');
